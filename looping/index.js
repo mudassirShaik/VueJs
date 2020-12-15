c427763a6cd1c493bdbce976d13f587cc0e0e1d7
mudassirShaik <75924368+mudassirShaik@users.noreply.github.com>
2020-12-15 21:16:01 +0530

    Add files via upload

---
+++ looping/index.js
@@ -0,0 +1,16 @@
+var app = new Vue({
+    el:'#app',
+    data:{
+      student:['Jim', 'Rob', 'Ryan', 'John'],
+      players:[
+          {name:'Joe',score:5},
+          {name:'Dan',score:3},
+          {name:'Josh',score:2},
+          {name:'Ford',score:7},
+      ],
+      status:true,
+    },
+    methods: {
+       
+    }
+});
